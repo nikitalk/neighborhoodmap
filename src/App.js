af3980e8e8db20776bba306e50b2d57771e1674c
nikitalk <nikita.lk@gmail.com>
2018-07-21 09:50:12 +0300

    refactor: fixing error with fetching

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ selectionLocation = (location) => {
       activeLocation: location,
       selected: true
     });
-    this.fetchFourSquare(this.state.activeLocation.idFourSquare)
+    
+    this.fetchFourSquare(location.idFourSquare)
 }
  
 unselectionLocation = () => {   
@@ -28,28 +29,35 @@ unselectionLocation = () => {
 }
 
 fetchFourSquare(id) {
-  fetch(`https://api.foursquare.com/v2/venues/${id}?client_id=5P0VNDSS1SVTAXPLHKA4SQDFASHIE1BHNAKMVAKYOEADYMGS&client_secret=DZFVTUHK3MPR0DWT22RJNO2GMWPBYM1EBXHPRTHFMLQ421EU&v=20180323`)
+  fetch(`https://api.foursquare.com/v2/venues/${id}?client_id=5P0VNDSS1SVTAXPLHKA4SQDFASHIE1BHNAKMVAKYOEADYMGS&client_secret=AUBY2AHOWPOUMUAIDWNPAMUAZVAQO3BPFJBRRIBQE5GIA50Q&v=20180323`)
     .then(res => res.json())
     .then(data => this.pushIntoInfo(data))
+     .catch(err => alert(`Unable to get data from FourSquare (${err})`))
 }
 
 pushIntoInfo(data) {
-  let html = '';
-  const object = data.response.venue
+
+  if (data.response.venue) {
+
+    const object = data.response.venue
+
   document.getElementById(this.state.activeLocation.idFourSquare).classList.add('selectedLocation')
-  if (this.state.previousLocation.idFourSquare !== undefined) document.getElementById(this.state.previousLocation.idFourSquare).classList.remove('selectedLocation')
-  if (object) {
-      
-      html = `
+  if (this.state.previousLocation.idFourSquare !== undefined) {
+    document.getElementById(this.state.previousLocation.idFourSquare).classList.remove('selectedLocation')
+    document.querySelector(`#info${this.state.previousLocation.idFourSquare}`).classList.add('unvisible')
+  }
+      const html = `
         <img src="${object.bestPhoto.prefix}300x200${object.bestPhoto.suffix}" alt="${object.name}">
         <div style="color: #${object.ratingColor};">${object.rating}</div>
         <div class="#selectedLocation">Address: ${object.location.formattedAddress[0]}</div>
         <div>Likes: ${object.likes.count}</div>
         <a href="${object.canonicalUrl}" target="_blank">Look at this on FourSquare</a>`
         document.querySelector(`#info${this.state.activeLocation.idFourSquare}`).innerHTML = html
+        document.querySelector(`#info${this.state.activeLocation.idFourSquare}`).classList.remove('unvisible')
     } else {
-        html= `<div> Unable to load information from FourSquare</div>`
+        const html = `<div> Unable to load information from FourSquare (${data.meta.errorDetail})</div>`
       document.querySelector(`#infos`).innerHTML = html
+        
       }
         
     }
